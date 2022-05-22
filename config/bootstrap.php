2c49ac73718180b4581be3c20330976e33d37586
Gabriel Cruz <gabrielcruz.mail@gmail.com>
2022-05-22 12:28:47 -0300

    Criando bootstrap da app

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,17 +1,16 @@
 <?php
 
-
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Hello worlds!");
     return $response;
 });
 
-$app->run();
+return $app;
