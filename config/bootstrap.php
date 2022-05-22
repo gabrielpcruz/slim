3894ce357328b858240df07c8dd98d4b48779758
Gabriel Cruz <gabrielcruz.mail@gmail.com>
2022-05-22 12:55:00 -0300

    Criando a injeção de dependencia

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,12 +1,30 @@
 <?php
 
+use DI\ContainerBuilder;
+use DI\Container;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\App;
 
+// Settings
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$settings = [
+    'settings' => function () {
+        return require __DIR__ . '/settings.php';
+    },
+    App::class => function (ContainerInterface $container) {
+        return AppFactory::createFromContainer($container);
+    }
+];
+
+$containerBuild = new ContainerBuilder(Container::class);
+$containerBuild->addDefinitions($settings);
+$container = $containerBuild->build();
+
+$app = $container->get(App::class);
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello worlds!");
---
+++ config/settings.php
@@ -0,0 +1,22 @@
+<?php
+
+// Configure defaults for the whole application.
+
+// Error reporting
+error_reporting(0);
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+
+// Timezone
+date_default_timezone_set('America/Sao_Paulo');
+
+// Settings
+$settings = [];
+
+// Path settings
+$settings['root'] = dirname(__DIR__);
+$settings['tests'] = $settings['root'] . '/tests';
+$settings['public'] = $settings['root'] . '/public';
+
+
+return $settings;
