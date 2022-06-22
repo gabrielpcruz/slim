f55de05e9f04826fbee2c81d20163b9248903a78
Gabriel Cruz <gabrielcruz.mail@gmail.com>
2022-06-21 21:08:11 -0300

    - Finalizando exemplo de command

--- src/Console/ConsoleExample.php
+++ src/Console/ConsoleExample.php
@@ -24,7 +24,26 @@ class ConsoleExample extends Console
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $message = "<info>Hello, I'm console command.</info>";
+        $message = "<info>Hello, I'm console command Info.</info>";
+
+        $output->writeln($message);
+
+        $message = "<comment>Hello, I'm console command Comment.</comment>";
+
+        $output->writeln($message);
+
+        $message = "<question>Hello, I'm console Question.</question>";
+
+        $output->writeln($message);
+
+        $message = "<error>Hello, I'm console command Error.</error>";
+
+        $output->writeln($message);
+
+        $link = 'https://github.com/gabrielpcruz/slim';
+
+        $message = "<href={$link}>I'm a example of link on console. Hold CTRL + Click-me</>";
+
         $output->writeln($message);
 
         return Command::SUCCESS;
