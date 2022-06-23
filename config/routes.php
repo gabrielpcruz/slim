dd8b2efa68460c8f57286b6349952fab563bd075
Gabriel Cruz <gabrielcruz.mail@gmail.com>
2022-06-22 21:14:26 -0300

    - Ajustando rotas

--- config/routes.php
+++ config/routes.php
@@ -1,24 +1,14 @@
 <?php
 
 use Slim\App;
-use Slim\Routing\RouteCollectorProxy;
 
 use App\Http\Site\Home;
 use App\Http\Api\Home as HomeApi;
 
 return function (App $app) {
     // Site
-    $app->group('/', function (RouteCollectorProxy $group) {
-        $group->get('', [Home::class, 'index']);
-
-        $group->post('', [Home::class, 'arroz']);
-    });
-//
-//    $app->get('/arroz', [Home::class, 'terra']);
+    $app->get('/', [Home::class, 'index']);
 
     // Api
-
-    $app->get('/api/v1/home', [HomeApi::class, 'index']);
-
-
+    $app->get('/v1/home', [HomeApi::class, 'index']);
 };
--- src/Http/Api/Home.php
+++ src/Http/Api/Home.php
@@ -3,8 +3,12 @@
 namespace App\Http\Api;
 
 use App\Http\ControllerApi;
+use App\Repository\Example\RiceRespository;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use ReflectionException;
 
 class Home extends ControllerApi
 {
@@ -12,11 +16,17 @@ class Home extends ControllerApi
      * @param Request $request
      * @param Response $response
      * @return Response
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     * @throws ReflectionException
      */
     public function index(Request $request, Response $response): Response
     {
-        return $this->responseJSON($response, [
-            'message' => 'response message'
-        ]);
+        $repository = $this->getRepositoryManager()->get(RiceRespository::class);
+
+        return $this->responseJSON(
+            $response,
+            $repository->all()->toArray()
+        );
     }
 }
