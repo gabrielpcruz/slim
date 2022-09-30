09a6241a95418447a7ec113dc89f28020a73e97e
Gabriel Cruz <gabrielcruz.mail@gmail.com>
2022-09-29 21:05:57 -0300

    Deixando pronto pra rodar no heroku

--- config/database/connections/sqlite.php
+++ config/database/connections/sqlite.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-        'driver' => 'sqlite',
-        'host' => 'localhost',
-        'database' => STORAGE_PATH . '/database/db.sqlite',
-        'username' => 'root',
-        'password' => 'root',
-        'charset' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-        'prefix' => '',
+    'driver' => 'sqlite',
+    'host' => 'localhost',
+    'database' => ':memory:',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
 ];
