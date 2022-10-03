dd0e369efe7d4c6429e64a449b8e20c5ad6c7d64
Gabriel Cruz <48037228+gabrielpcruz@users.noreply.github.com>
2022-10-03 12:43:37 -0300

    Update DefaultErrorHandler.php

--- src/Handler/DefaultErrorHandler.php
+++ src/Handler/DefaultErrorHandler.php
@@ -52,7 +52,7 @@ class DefaultErrorHandler implements ErrorHandlerInterface
         $response = new Response();
 
         $message = $exception->getMessage();
-        $code = $exception->getCode() ? $exception->getCode() : 500;
+        $code = $exception->getCode() ? intval($exception->getCode()) : 500;
 
 
         if ($this->isApi($request)) {
